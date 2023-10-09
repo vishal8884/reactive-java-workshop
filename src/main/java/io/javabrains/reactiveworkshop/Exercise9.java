853c67cddf9ae705aa32f5fa6bee044354a54940
vishal <vishalambekar459@gmail.com>
2023-10-09 23:25:13 +0530

    Exercise 9 added

--- src/main/java/io/javabrains/reactiveworkshop/Exercise9.java
+++ src/main/java/io/javabrains/reactiveworkshop/Exercise9.java
@@ -1,6 +1,9 @@
 package io.javabrains.reactiveworkshop;
 
 import java.io.IOException;
+import java.util.List;
+
+import reactor.core.publisher.Mono;
 
 public class Exercise9 {
 
@@ -11,12 +14,13 @@ public class Exercise9 {
 
         // Print size of intNumbersFlux after the last item returns
         // TODO: Write code here
+    	//ReactiveSources.intNumbersFlux().count().subscribe(num -> System.out.println(num));
 
         // Collect all items of intNumbersFlux into a single list and print it
-        // TODO: Write code here
+    	//ReactiveSources.intNumbersFlux().collectList().subscribe(num -> System.out.println(num));
 
         // Transform to a sequence of sums of adjacent two numbers
-        // TODO: Write code here
+    	ReactiveSources.intNumbersFlux().buffer(3).subscribe(num -> System.out.println(num ));
 
         System.out.println("Press a key to end");
         System.in.read();
