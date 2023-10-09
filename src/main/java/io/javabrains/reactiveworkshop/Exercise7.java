0257ac493e47aa3eae0d0f65baf8043cb6e3e1d7
vishal <vishalambekar459@gmail.com>
2023-10-09 22:48:01 +0530

    Exercise 7 added

--- src/main/java/io/javabrains/reactiveworkshop/Exercise7.java
+++ src/main/java/io/javabrains/reactiveworkshop/Exercise7.java
@@ -10,25 +10,57 @@ public class Exercise7 {
         // Use ReactiveSources.intNumberMono() and ReactiveSources.userMono()
 
         // Print all values from intNumbersFlux that's greater than 5
-        // TODO: Write code here
+//    	ReactiveSources.intNumbersFlux()
+//    	.filter(num -> num > 5)
+//    	.subscribe(num -> System.out.println(num));
 
+    	
+    	
         // Print 10 times each value from intNumbersFlux that's greater than 5
-        // TODO: Write code here
+//    	ReactiveSources.intNumbersFlux()
+//    	.filter(num -> num > 5)
+//    	.repeat(10)
+//    	.subscribe(num -> System.out.println(num));
 
+    	
+    	
         // Print 10 times each value from intNumbersFlux for the first 3 numbers emitted that's greater than 5
-        // TODO: Write code here
+//    	ReactiveSources.intNumbersFlux()
+//    	.filter(num -> num > 5)
+//    	.take(3)
+//    	.repeat(10)
+//    	.subscribe(num -> System.out.println(num));
 
+    	
+    	
         // Print each value from intNumbersFlux that's greater than 20. Print -1 if no elements are found
-        // TODO: Write code here
+//    	ReactiveSources.intNumbersFlux()
+//    	.filter(num -> num > 20)
+//    	.defaultIfEmpty(-1)
+//    	.subscribe(num -> System.out.println(num));
 
+    	
+    	
         // Switch ints from intNumbersFlux to the right user from userFlux
-        // TODO: Write code here
+    	
+//    	ReactiveSources.intNumbersFlux()
+//    	.flatMap(id -> ReactiveSources.userFlux().filter(user -> user.getId() == id))
+//    	.subscribe(user -> System.out.println(user));
+    	
 
+    	
+    	
         // Print only distinct numbers from intNumbersFluxWithRepeat
-        // TODO: Write code here
+//    	ReactiveSources.intNumbersFluxWithRepeat() 
+//    	.distinct()
+//    	.repeat(2)
+//    	.subscribe(num -> System.out.println(num));
 
         // Print from intNumbersFluxWithRepeat excluding immediately repeating numbers
-        // TODO: Write code here
+    	ReactiveSources.intNumbersFluxWithRepeat() 
+    	.distinctUntilChanged()
+    	.repeat(2)
+    	.subscribe(num -> System.out.println(num));
 
         System.out.println("Press a key to end");
         System.in.read();
